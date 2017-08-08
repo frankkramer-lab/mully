e7b22fe34a3ddcfea60153a3401c83ba15f7a21e
Zaynab <zaynabhassanhammoud@gmail.com>
2017-08-08 13:33:57 +0200

    Add print info to demo.

--- R/mully_demo.R
+++ R/mully_demo.R
@@ -1,4 +1,3 @@
-
 demo <- function() {
 
   g <- mully("MyFirstMully",direct = F)
@@ -6,13 +5,21 @@ demo <- function() {
   g <- addLayer(g, c("Gene", "Drug", "Drug", "Disease"))
 
   g=addNode(g,"d1","disease",attributes=list(type="t1"))
+  print("Node d1 added as disease")
   g=addNode(g,"d2","disease",attributes=list(type="t1"))
+  print("Node d2 added as disease")
   g=addNode(g,"d3","disease",attributes=list(type="t1"))
+  print("Node d3 added as disease")
   g=addNode(g,"dr1","drug",attributes=list(effect="strong"))
+  print("Node dr1 added as drug")
   g=addNode(g,"dr2","drug",attributes=list(effect="strong"))
+  print("Node dr2 added as drug")
   g=addNode(g,"dr3","drug",attributes=list(effect="moderate"))
+  print("Node dr3 added as drug")
   g=addNode(g,"g1","gene",attributes=list(desc="AF"))
+  print("Node g1 added as gene")
   g=addNode(g,"g2","gene",attributes=list(desc="BE"))
+  print("Node g2 added as gene")
 
   #See vertices attributes
   as.data.frame(get.vertex.attribute(g))
