cdc92a1f3a20814e8b75133d7e9ef54b8ee7bcad
Zaynab <zaynabhassanhammoud@gmail.com>
2018-08-30 12:33:20 +0200

    Move from mully.R to mully_constructor.R.

---
+++ R/mully_constructor.R
@@ -0,0 +1,45 @@
+########### Constructor ##################
+
+#' Create an empty multilayered graph
+#'
+#' @param name The name to be assigned to the graph.
+#' @param direct A boolean value, if the graph is directed or not. By default TRUE.
+#'
+#' @return The created multilayered graph.
+#' @export
+mully <- function(name = NA, direct = TRUE) {
+  #Create the layers Indexation Table
+  #Vector for the layers indexation
+  #One row is not enough (in case we delete layers)
+  layersVect = data.frame(ID = is.numeric(c()),
+                          Name = is.character(c()),
+                          NameLower = is.character(c()))
+  #Delete first entry F F F
+  layersVect = layersVect[-1,]
+
+  #Create an empty igraph instance
+  g <- graph.empty(directed = direct)
+
+  #Assign the attributes to the igraph instance
+  g$name = name
+  #Last inserted layer number
+  g$iLayer = 0
+  #DataFrame that contains the layers
+  g$layers = layersVect
+
+  #name the class
+  class(g) = c("mully", class(g))
+  return(g)
+}
+
+is.mully<-function(g){
+  if(!is.igraph(g) || is.na(g$layers) || is.na(g$iLayer))
+    return(F)
+  return(T)
+}
+
+
+# #TODO overwrite igraph.print
+# mully.print<-function(g){
+#
+# }
